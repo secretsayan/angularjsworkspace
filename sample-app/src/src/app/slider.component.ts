1ab512b879e727e933c45867405d3a224e4f4077
sayan goswami <secretsayan@gmail.com>
2018-06-24 23:12:52 +0530

    First app stackblitz

---
+++ sample-app/src/src/app/slider.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mycomponent',
+  template: './slider.component.html',
+  styles: [`h4 { font-family: Lato; }`]
+})
+export class MyComponent {
+  slider: string;
+
+}
